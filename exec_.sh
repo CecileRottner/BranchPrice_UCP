277f292de82d18952b175f603a5c0035c6c51c8d
Cecile ROTTNER <rottner@poleia.lip6.fr>
2018-05-24 19:11:38 +0200

    résultats gradients

--- exec_.sh
+++ exec_.sh
@@ -3,16 +3,16 @@ rm result.txt
 
 nom=result.txt
 
-n=10
-sym=0
-demand_type=3
+n=20
+sym=3
+demand_type=4
 cat01=0
 
 
-dossier=data/
+dossier=data/sym=site/
 
 for T in 24 ; do
-  for id in {1..10}; do
+  for id in {1..20}; do
       ./bin/SCIP_UCP_BP.linux.x86_64.gnu.opt.cpx 1 $dossier $n $T 1 $demand_type $sym $cat01 1 $id $nom
   done
   printf "\n" >> result.txt
@@ -22,3 +22,33 @@ done
 printf "\n" >> result.txt
 printf "\n" >> result.txt
 
+n=30
+sym=4
+
+for T in 24 ; do
+  for id in {1..20}; do
+      ./bin/SCIP_UCP_BP.linux.x86_64.gnu.opt.cpx 1 $dossier $n $T 1 $demand_type $sym $cat01 1 $id $nom
+  done
+  printf "\n" >> result.txt
+  printf "\n" >> result.txt
+done
+
+printf "\n" >> result.txt
+printf "\n" >> result.txt
+
+
+dossier=data/sym_C_site/
+
+n=20
+sym=3
+
+for T in 24 ; do
+  for id in {1..20}; do
+      ./bin/SCIP_UCP_BP.linux.x86_64.gnu.opt.cpx 1 $dossier $n $T 1 $demand_type $sym $cat01 1 $id $nom
+  done
+  printf "\n" >> result.txt
+  printf "\n" >> result.txt
+done
+
+printf "\n" >> result.txt
+printf "\n" >> result.txt
