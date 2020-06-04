4ec8bcf7b144aa568fae329bde438bf80c474a66
Cécile Rottner <cecile.rottner@edf.fr>
2020-06-04 12:42:49 +0200

    Clean repository

--- cplex_scip_dir_lip6.mk
+++
@@ -1,3 +0,0 @@
-VARSCIPDIR       := /home/rottner/Programmes/scipoptsuite-5.0.1/scip
-VARCPLEXDIR      := /home/rottner/Programmes/Cplex/cplex
-VARCONCERTDIR    := /home/rottner/Programmes/Cplex/concert
